b6d9b9378f540698a22f60b199d80a1fa9b0243e
nicewei77-lang <nicewei77@gmail.com>
2026-06-11 01:25:05 +0900

    feat: config.py pydantic setting으로 .env의 데이터 가져올 수 있게 하기
    docs: 디렉터리 정리 및 코칭 파일 보완

---
+++ backend/app/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# 상속할 부모 클래스
+class Settings(BaseSettings):
+    # 데이터를 받을 필드: type hint
+    database_url: str
+    jwt_secret: str
+    
+    model_config = SettingsConfigDict(env_file=".env")
+# 다른 곳에서 쓸 객체 인스턴스 만들기
+settings = Settings()
+    
+    
